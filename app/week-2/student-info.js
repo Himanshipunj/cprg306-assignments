ca5ec568873bd8a2ebd51d349a9e9b5e97f9d85e
Himanshipunj <166560799+Himanshipunj@users.noreply.github.com>
2025-01-20 19:07:52 -0700

    Added student-info component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Himanshi Punj</h1>
+      <p>Check out my GitHub: 
+        <Link href="https://github.com/Himanshipunj">
+        </Link>
+      </p>
+    </div>
+  );
+}
+
+
+
