cc550ea5c7b76f42f5ad7af9b394d5b5489dd4df
Himanshipunj <166560799+Himanshipunj@users.noreply.github.com>
2025-01-22 16:03:02 -0700

    Added Week 3 Assignment

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ export default function Page() {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>Link to week 2: <a href="week-2">Week 2</a> </p>
+      <p>Link to week 3: <a href="week-3">Week 3</a> </p>
     </div>
   );
 }
---
+++ app/week-3/page.js
@@ -0,0 +1,13 @@
+import ItemList from './item-list';
+
+export default function Page() {
+    return (
+        <main className="max-w-2xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-10">
+            <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Shopping List</h1>
+            <ItemList />
+        </main>
+    );
+}
+
+
+
